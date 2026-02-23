1f51fec29ff543396000f240a4d3576ca30eb52a
tarunlab-io <gtarunaditya@gmail.com>
2026-02-23 09:41:39 +0530

    UC2: Print OOPS as 7-line ASCII banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,21 @@
 /**
  * OOPSBannerApp
  *
- * UC1: Print literal text "OOPS" to console
+ * UC2: Display "OOPS" as a 7-line ASCII banner using '*' and spaces
  *
  * @author Tarun
- * @version 1.0
+ * @version 2.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" *****   *****   *****   *****");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println(" *****   *****   *****   *****");
+        System.out.println("*       *       *     *      *");
+        System.out.println("*       *       *     *      *");
+        System.out.println("*       *       *****   *****");
     }
 }
