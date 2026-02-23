0e5db0fbca7981c84fadb7dd5f8f1d582000bf87
tarunlab-io <gtarunaditya@gmail.com>
2026-02-23 09:26:52 +0530

    UC1: Print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC1: Print literal text "OOPS" to console
+ *
+ * @author Tarun
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
